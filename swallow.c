e7ec81861f05431254889aa1fc68f582ed5ba005
Spenser Truex <truex@equwal.com>
2023-05-25 20:21:40 -0300

    Style edits
    Bonus points: no string lib. The real K&R.

--- swallow.c
+++ swallow.c
@@ -1,7 +1,6 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 void usage() {
     printf("swallow app_name [arguments]\n");
@@ -14,21 +13,19 @@ int main(int argc, char** argv) {
     }
 
     char *command;
-    int maxsize, i;
-    for(maxsize=0,i=1; argv[i]; i++) {
-      maxsize += strlen(argv[i]);
-    }
+    int maxsize,i,j;
+
+    for(maxsize=0,i=1; argv[i]; i++)
+      for(j=0; argv[i][j];j++)
+        maxsize++;
 
     command = (char*)malloc(maxsize);
     char *c = command;
 
     for (int i = 1; argv[i]; i++) {
-      for(int j=0; argv[i][j]; j++) {
-        *command=argv[i][j];
-        command++;
-      }
-     *command = ' ';
-     command++;
+      for(int j=0; argv[i][j]; j++)
+        *(command++) = argv[i][j];
+     *(command++) = ' ';
     }
     command = c;
     printf("%s", command);
@@ -38,7 +35,7 @@ int main(int argc, char** argv) {
     Window win;
     Display* dis = XOpenDisplay(0);
 
-    if (dis == NULL) {
+    if (!dis) {
         printf("Failed to open display!\n");
         return 0;
     }
@@ -56,7 +53,7 @@ int main(int argc, char** argv) {
     // launch the application
     int res = system(command);
 
-    if (res != 0) {
+    if (res) {
         printf("Application exited with %i\n", res);
     }
 
